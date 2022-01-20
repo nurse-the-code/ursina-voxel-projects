ca726cae3bc57d506a42e336ea5ab2ffdc3fb6e1
Malachi Gruenhagen <68450431+nurse-the-code@users.noreply.github.com>
2022-01-19 23:38:28 -0500

    syntax changes with PyCharm

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import ursina as u
 from ursina.color import random_color
-from ursina.prefabs.first_person_controller import FirstPersonController # not imported by default
+from ursina.prefabs.first_person_controller import FirstPersonController  # not imported by default
 from voxel import Voxel
 
 app = u.Ursina()
@@ -8,10 +8,8 @@ app = u.Ursina()
 # Create an 16 x 16 voxel platform on the horizontal plane
 for z in range(16):
     for x in range(16):
-        voxel = Voxel(block_position = (x, 0, z))
-
+        voxel = Voxel(block_position=(x, 0, z))
 
 player = FirstPersonController()
 
-
-app.run() # CTRL + ALT + ESC to stop
+app.run()  # CTRL + ALT + ESC to stop
